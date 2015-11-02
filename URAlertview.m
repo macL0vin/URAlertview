54274737ef6155a38b03e525afe123c125d89069
Uahit Ramazanov <west-t@hotmail.com>
2015-11-02 12:19:01 +0500

    Update URAlertview.m

--- URAlertview.m
+++ URAlertview.m
@@ -9,14 +9,6 @@
 
 @implementation URAlertview
 
-/*
-// Only override drawRect: if you perform custom drawing.
-// An empty implementation adversely affects performance during animation.
-- (void)drawRect:(CGRect)rect {
-    // Drawing code
-}
-*/
-
 - (id)initWithFrame:(CGRect)frame
               title:(NSString*)title
         buttonColor:(UIColor*)color
