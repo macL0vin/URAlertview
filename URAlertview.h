94a35cce96d8a172ba7b83972c780fdda15badbc
Uahit Ramazanov <kmgp@MacBook-Pro.local>
2015-11-02 11:52:34 +0500

    first commit
    test version

---
+++ URAlertview.h
@@ -0,0 +1,23 @@
+//
+//  URAlertview.h
+//  Workshare
+//
+//  Created by Uahit Ramazanov on 11/2/15.
+//  Copyright © 2015 kmgpIT. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+typedef void (^OKButtonClick)(UIView *_self);
+
+@interface URAlertview : UIView
+
+@property (nonatomic, copy) OKButtonClick onTap;
+
+// Initialization
+- (id)initWithFrame:(CGRect)frame
+              title:(NSString*)title
+        buttonColor:(UIColor*)color
+              onTap:(OKButtonClick)block;
+
+@end
